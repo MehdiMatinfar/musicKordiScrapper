7e766e2b5e7bed902d141e0d1835b820bbd981ed
MehdiMatinfar <65761886+MehdiMatinfar@users.noreply.github.com>
2022-08-20 13:52:52 +0430

    Update musicKordi-Scrapper.py

--- musicKordi-Scrapper.py
+++ musicKordi-Scrapper.py
@@ -3,18 +3,15 @@ import re
 import os.path
 from requests_html import HTMLSession
 
-base_url = "https://musickordi.com/singer/%D8%B3%DB%8C%D9%88%D8%A7%D9%86-%DA%AF%D8%A7%DA%AF%D9%84%DB%8C.html"
-singer="sayvanGagli"
-dir_name = "./"+singer+"/"
-
 
 def findLink(link):
     regex = r"(https:\/\/dl\.musickordi.com\/).*(mp3)"
 
-    links = []
+    
     matches = re.finditer(regex, link, re.MULTILINE)
 
     for matchNum, match in enumerate(matches, start=1):
+        
         print("Match {matchNum} was found at {start}-{end}: {match}".format(
             matchNum=matchNum, start=match.start(), end=match.end(), match=match.group()))
 
@@ -24,7 +21,7 @@ def findLink(link):
         # print("Group {groupNum} found at {start}-{end}: {group}".format(groupNum=groupNum,
         #    start=match.start(groupNum), end=match.end(groupNum), group=match.group(groupNum)))
         list.append(match.group(groupNum))
-        
+
     return list
 
 
@@ -40,50 +37,75 @@ def downloadMusic(musicLink, name):
 
 
 def call(url):
+    
     s = HTMLSession()
     
     response = s.get(url, headers={
         'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.0.0 Safari/537.36'})
     
     audio = response.html.find('audio.default-player')
-    print("-"*50)
+    
+    print("-" * 50)
     print("Downloading Started ...\n")
     
     for item in audio:
         
         music = item.find('source', first=True).attrs['src']
-        print(music)
-        #  downloadMusic(str(music), music[music.rindex("/") + 1:])
-        print(music[music.rindex("/") + 1:],"")
         
+        print(music)
+        
+        downloadMusic(str(music), music[music.rindex("/") + 1:])
+        print(music[music.rindex("/") + 1:], "")
+
+
 def link_generator(page_number):
-
+    
     print(f"page : #{page_number}")
-    return base_url+"/page/{}/".format(page_number)
-
+    return base_url + "/page/{}/".format(page_number)
 
 
 def fetchPages():
+    
     s = HTMLSession()
+    
     response = s.get(base_url, headers={
         'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.0.0 Safari/537.36'})
-    pages = response.html.find('div.pagination')
-    list = pages[0].find('a')
-    last_page=int(list[len(list)-2].text)
 
-    for i in range(1,last_page):
-        call(link_generator(i))
+    pages = response.html.find('div.pagination')
+
+    if len(pages) !=0:
+
+        list = pages[0].find('a')
+        last_page = int(list[len(list) - 2].text)
+
+        for i in range(1, last_page):
+            call(link_generator(i))
+
+    else:
+
+        call(base_url)
+
 
 def getSingerName():
-    link="https://dl.musickordi.com/Saiwan-Gagli/Saiwan-Gagli_Narin-Narin-Yari-Jwanm[128].mp3"
+    
+    link = "https://dl.musickordi.com/Saiwan-Gagli/Saiwan-Gagli_Narin-Narin-Yari-Jwanm[128].mp3"
     regex = r"(https:\/\/dl\.musickordi.com\/).*(\/)"
 
-    name=""
+    name = ""
+    
     matches = re.finditer(regex, link, re.MULTILINE)
+    
     for matchNum, match in enumerate(matches, start=1):
-
-         name=match.group()
+        
+        name = match.group()
+        
     print(name)
 
-#getSingerName()
+
+# getSingerName()
+
+base_url = str(input("Page Link: "))
+singer = str(input("Folder Name: "))
+dir_name = "./" + singer + "/"
+
 fetchPages()
